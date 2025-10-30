a918d9874ccec903753648d949046c1093845ff8
Wafiq1977 <mochwafiqizna@gmail.com>
2025-10-30 13:57:59 +0700

    first commit

---
+++ src/main/java/savoraapp/com/savoraapp/SavoraappApplication.java
@@ -0,0 +1,13 @@
+package savoraapp.com.savoraapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SavoraappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SavoraappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/savoraapp/com/savoraapp/SavoraappApplicationTests.java
@@ -0,0 +1,13 @@
+package savoraapp.com.savoraapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SavoraappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
